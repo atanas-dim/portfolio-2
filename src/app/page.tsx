93ba48f4bbb10736761c46c2cbfa949053188cff
Atanas Dimitrov <hi.atanasdim@gmail.com>
2024-11-22 19:27:32 +0000

    Create page sections with glossy text effect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,100 +2,68 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+    <>
+      <main className="size-full min-h-fit flex flex-col mx-auto max-w-screen-md p-6 gap-28 glossy">
+        <section className="flex flex-col">
+          <div className="flex flex-col">
+            <h1 className="text-4xl lg:text-6xl font-black leading-tight">
+              <span className="">Atanas Dimitrov</span>
+            </h1>
+            <h2 className="text-4xl lg:text-6xl font-black leading-tight">
+              <span className="">React Developer</span>{" "}
+              <span className="emoji">🪄</span>
+            </h2>
+          </div>
+          <ul className="flex text-4xl">
+            {["👨‍💻", "📚", "🎨", "🎸"].map((emoji, index) => {
+              return (
+                <li key={"link-" + index}>
+                  <a className="emoji size-12 items-center justify-center flex">
+                    {emoji}
+                  </a>
+                </li>
+              );
+            })}
+          </ul>
+        </section>
+        <section className="flex flex-col gap-4">
+          <h2 className="text-3xl lg:text-5xl font-bold ">About me</h2>
+          <p className="text-xl lg:text-2xl">
+            I am a React developer with a passion for creating beautiful and
+            functional web applications. I love to learn new things and share my
+            knowledge with others.
+          </p>
+        </section>
+        <section className="flex flex-col gap-4">
+          <h2 className="text-3xl lg:text-5xl font-bold ">Projects</h2>
+          <ul className="gap-4 grid grid-cols-1 lg:grid-cols-3">
+            {["Project 1", "Project 2", "Project 3"].map((project, index) => {
+              return (
+                <li key={"project-" + index}>
+                  <div className="flex flex-col rounded-3xl p-2 gap-1 border-2 border-black">
+                    <Image
+                      src="https://via.placeholder.com/300"
+                      alt=""
+                      className="aspect-square w-full rounded-2xl"
+                      width={300}
+                      height={300}
+                    />
+                    <div className="flex flex-col p-2">
+                      <p className="text-2xl font-bold">{project}</p>
+                      <p className="text-base">Description of {project}</p>
+                    </div>
+                  </div>
+                </li>
+              );
+            })}
+          </ul>
+        </section>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+      <footer className="p-6">
+        <p className="text-xs lg:text-sm text-black">
+          2024 Created with NextJS, TailwindCSS and GSAP
+        </p>
       </footer>
-    </div>
+    </>
   );
 }
