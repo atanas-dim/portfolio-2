b77712682070c1e3b9467f138a3bf149627e8720
Atanas Dimitrov <hi.atanasdim@gmail.com>
2025-07-19 16:57:11 +0100

    Refactor glossy animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,45 +6,44 @@ import gsap from 'gsap'
 import Image from 'next/image'
 import { FC, useEffect, useState } from 'react'
 
-const ELEMENTS = 'main,section,h1,h2,p,.social-link,.card,.tool'
+const ELEMENTS = ' h1,h2,p,.social-link,.card,.tool'
 
 export default function Home() {
   useEffect(() => {
-    gsap.set(ELEMENTS, {
+    const els = gsap.utils.selector('body')(ELEMENTS)
+
+    gsap.set(els, {
       opacity: 0,
       y: 6,
       transition: 'none',
     })
 
-    gsap.to(ELEMENTS, {
-      opacity: 1,
-      y: 0,
-      duration: 0.5,
-      stagger: 0.1,
-      onComplete: () => {
-        gsap.set(ELEMENTS, {
-          clearProps: 'transition,transform',
-        })
-      },
+    els.forEach((el, index) => {
+      gsap.to(el, {
+        opacity: 1,
+        y: 0,
+        duration: 0.5,
+        delay: 0.1 * index,
+        onComplete: () => {
+          gsap.set(el, {
+            clearProps: 'transition,transform',
+          })
+        },
+      })
     })
   }, [])
 
   return (
     <>
       <div role="presentation" className="grainy pointer-events-none absolute inset-0 z-1000" />
-      <div
-        role="presentation"
-        className="pointer-events-none absolute inset-0 z-1000 overflow-hidden opacity-100 mix-blend-lighten">
-        <div role="presentation" className="bg-glossy pointer-events-none absolute inset-0 h-full w-[200vw]" />
-      </div>
 
-      <main className="mx-auto mb-16 flex size-full min-h-fit max-w-5xl flex-col gap-28 px-6 py-8 opacity-0 motion-reduce:animate-none lg:py-16">
-        <section className="flex flex-col gap-4">
+      <main className="mx-auto mb-16 flex size-full min-h-fit max-w-5xl flex-col gap-28 px-6 py-8 motion-reduce:animate-none lg:py-16">
+        <section className="relative flex flex-col gap-4">
           <div className="flex flex-col">
-            <h1 className="text-glossy font-souvenir text-3xl font-extrabold whitespace-nowrap lg:text-6xl">
+            <h1 className="font-souvenir block text-3xl font-extrabold whitespace-nowrap lg:text-6xl">
               Atanas Dimitrov <span className="text-2xl text-black lg:text-5xl lg:leading-tight">🪄</span>
             </h1>
-            <h2 className="text-glossy font-souvenir text-3xl font-extrabold whitespace-nowrap lg:text-6xl">
+            <h2 className="font-souvenir text-3xl font-extrabold whitespace-nowrap lg:text-6xl">
               React Developer <span className="text-2xl text-black lg:text-5xl lg:leading-tight">⚛️</span>
             </h2>
           </div>
@@ -66,16 +65,18 @@ export default function Home() {
               )
             })}
           </ul>
+          <GlossyOverlay />
         </section>
-        <section className="flex flex-col gap-4">
-          <h2 className="text-glossy font-souvenir text-3xl font-extrabold lg:text-5xl lg:leading-tight">About me</h2>
+        <section className="relative flex flex-col gap-4">
+          <h2 className="font-souvenir text-3xl font-extrabold lg:text-5xl lg:leading-tight">About me</h2>
           <p className="max-w-180 text-xl lg:text-2xl">
             I’m a React developer with a passion for creating beautiful and functional web applications. I’m always
             excited to experiment with new ideas and technologies.
           </p>
+          <GlossyOverlay />
         </section>
         <section className="flex flex-col gap-8">
-          <h2 className="text-glossy font-souvenir text-3xl font-extrabold lg:text-5xl lg:leading-tight">Projects</h2>
+          <h2 className="font-souvenir text-3xl font-extrabold lg:text-5xl lg:leading-tight">Projects</h2>
           <ul className="2xs:grid-cols-2 grid grid-cols-1 gap-4 md:grid-cols-3">
             {PROJECTS.map((project, index) => {
               return (
@@ -91,8 +92,8 @@ export default function Home() {
             })}
           </ul>
         </section>
-        <section className="flex flex-col gap-4">
-          <h2 className="text-glossy font-souvenir text-3xl font-extrabold lg:text-5xl lg:leading-tight">Tools</h2>
+        <section className="relative flex flex-col gap-4">
+          <h2 className="font-souvenir text-3xl font-extrabold lg:text-5xl lg:leading-tight">Tools</h2>
           <p className="mb-6 text-xl lg:text-2xl">I use the following technologies to create my projects:</p>
           <ul className="xs:grid-rows-8 grid w-fit grid-flow-col grid-rows-12 gap-x-6 gap-y-2 sm:gap-x-10 md:grid-rows-6">
             {TOOLS.map((tool, index) => {
@@ -103,14 +104,16 @@ export default function Home() {
               )
             })}
           </ul>
+          <GlossyOverlay />
         </section>
       </main>
-      <footer className="w-full">
+      <footer className="relative w-full">
         <div className="mx-auto max-w-5xl px-6">
           <p className="border-t-3 border-t-red-950/10 py-6 text-xs font-bold lg:text-sm">
             {new Date().getFullYear()} | Created with NextJS, TailwindCSS and GSAP
           </p>
         </div>
+        <GlossyOverlay />
       </footer>
     </>
   )
@@ -142,10 +145,10 @@ const Card: FC<CardProps> = ({ project }) => {
       }}>
       <div
         role="presentation"
-        className="mask-card-shadow pointer-events-none absolute inset-0 top-14 scale-60 rounded-3xl bg-rose-950/40 opacity-100 blur-xl transition-all duration-500 ease-linear group-hover:scale-110 group-hover:opacity-100 group-active:scale-110 group-active:opacity-100"
+        className="pointer-events-none absolute inset-0 top-14 scale-60 rounded-3xl bg-rose-950/40 opacity-100 blur-xl transition-all duration-500 ease-linear group-hover:scale-110 group-hover:opacity-100 group-active:scale-110 group-active:opacity-100"
       />
       <div
-        className="@container relative flex size-full flex-col gap-1 rounded-3xl border-2 border-rose-950 bg-rose-100 p-2 group-hover:bg-rose-50"
+        className="@container relative flex size-full flex-col gap-1 overflow-hidden rounded-3xl border-2 border-rose-950 bg-rose-100 p-2 group-hover:bg-rose-50"
         onPointerEnter={handlePointerEnter}>
         <div className="relative flex aspect-square w-full shrink-0 items-center justify-center overflow-hidden rounded-2xl border-2 border-rose-950">
           <Image
@@ -159,7 +162,7 @@ const Card: FC<CardProps> = ({ project }) => {
           <span className="absolute inset-0 bg-rose-200 mix-blend-color" />
         </div>
         <div className="flex h-full flex-col p-2">
-          <span className="text-glossy font-souvenir mb-1 text-xl font-extrabold @2xs:text-2xl">{project.title}</span>
+          <span className="font-souvenir mb-1 text-xl font-extrabold @2xs:text-2xl">{project.title}</span>
           <span className="mb-3 h-12 text-sm @2xs:text-base">{project.technologies}</span>
           <div className="mt-auto flex gap-2">
             {project.links.map((link, index) => {
@@ -176,7 +179,16 @@ const Card: FC<CardProps> = ({ project }) => {
             })}
           </div>
         </div>
+        <GlossyOverlay />
       </div>
+    </div>
+  )
+}
+
+const GlossyOverlay: FC = () => {
+  return (
+    <div role="presentation" className="pointer-events-none absolute inset-0 z-1 overflow-hidden mix-blend-lighten">
+      <div role="presentation" className="glossy pointer-events-none absolute inset-0 h-full w-[200vw]" />
     </div>
   )
 }
