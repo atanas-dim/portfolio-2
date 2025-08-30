ab06bea0250c537f10634b185305e43b5bbb4607
Atanas Dimitrov <hi.atanasdim@gmail.com>
2025-08-30 21:05:43 +0100

    Add test alerts

--- src/components/PageAnimations.tsx
+++ src/components/PageAnimations.tsx
@@ -90,23 +90,27 @@ export default function PageAnimations() {
     const enableMotion = () => {
       // Remove any existing animations
       gsap.getTweensOf('.bg-image').forEach((tween) => tween.kill())
-
+      alert('ENABLE')
       if (
         typeof DeviceOrientationEvent !== 'undefined' &&
         typeof (DeviceOrientationEvent as any).requestPermission === 'function'
       ) {
+        alert('ENABLE 1')
         // iOS 13+ requires permission
         ;(DeviceOrientationEvent as any)
           .requestPermission()
           .then((response: string) => {
             if (response === 'granted') {
+              alert('GRANTED')
               window.addEventListener('deviceorientation', handleOrientation, true)
             } else {
+              alert('DENIED')
               console.warn('Permission denied for device orientation')
             }
           })
           .catch(console.error)
       } else {
+        alert('ENABLE 2')
         // Non-iOS mobile or Android
         window.addEventListener('deviceorientation', handleOrientation, true)
       }
@@ -126,6 +130,7 @@ export default function PageAnimations() {
     const isMobile = isIOS || isAndroid
 
     if (isMobile) {
+      alert('MOBILE')
       const gestureOptions = { once: true }
       document.addEventListener('click', enableMotion, gestureOptions)
       document.addEventListener('touchstart', enableMotion, gestureOptions)
